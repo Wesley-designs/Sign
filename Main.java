90f4890ae90ff26c87deb0235d5f3ff94b5d8b31
Wesley-designs <jiewesleyd2789@nycstudents.net>
2025-02-03 14:31:34 +0000

    e empty# Please enter the commit message for your changes. Lines starting

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Sign sign1 = new Sign("ABC222DE", 3);
+        System.out.println(sign1.NumberOfLines());
+        System.out.println(sign1.getLines());
+        Sign sign2 = new Sign("ABCD", 10);
     }
 }
--- Sign.java
+++ Sign.java
@@ -1,13 +1,31 @@
 public class Sign {
-    public Sign(String line, int width) {
+    private String line;
+    private int totallines;
+    private int width;
 
+    public Sign(String line, int width) {
+        this.line = line;
+        this.width = width;
+        int extraline = 0;
+        if (line.length()%width != 0) extraline = 1;
+        totallines = line.length()/width + extraline;
     }
 
     public int NumberOfLines() {
-
+        return totallines;
     }
 
     public String getLines() {
-        
+        String ogline = line;
+        String sign = "";
+        for (int i=0; i<line.length()/width; i++) {
+            for (int o=0; o<width; o++) {
+                sign += ogline.substring(o,o+1);
+            }
+            ogline = ogline.substring(width,ogline.length());
+            sign += ";";
+        }
+        sign += ogline;
+        return sign;
     }
 }
